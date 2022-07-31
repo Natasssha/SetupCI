55d90b11d4c619d16cf20147ea684b22c0d91ea7
Natalia <nika_pozitivka@mail.ru>
2022-07-31 11:43:25 +0300

    Test

--- src/test/java/ru/netology/rest/MobileBankApiTestV2.java
+++ src/test/java/ru/netology/rest/MobileBankApiTestV2.java
@@ -1,2 +1,30 @@
-package ru.netology.rest;public class MobileBankApiTestV2 {
+package ru.netology.rest;
+
+import io.restassured.http.ContentType;
+import io.restassured.module.jsv.JsonSchemaValidator;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+
+class MobileBankApiTestV2 {
+
+    @Test
+    void shouldReturnDemoAccounts() {
+        // Given - When - Then
+        // Предусловия
+        given()
+                .baseUri("http://localhost:9999/api/v1")
+                // Выполняемые действия
+                .when()
+                .get("/demo/accounts")
+                // Проверки
+                .then()
+                .statusCode(200)
+                // .header("Content-Type", "application/json; charset=UTF-8")
+                // специализированные проверки - лучше
+                .contentType(ContentType.JSON)
+                // static import для JsonSchemaValidator.matchesJsonSchemaInClasspath
+                .body(JsonSchemaValidator.matchesJsonSchemaInClasspath("accounts.schema.json"))
+        ;
+    }
 }
--- src/test/java/ru/netology/rest/MobileBankApiTestV4.java
+++ src/test/java/ru/netology/rest/MobileBankApiTestV4.java
@@ -1,2 +1,28 @@
-package ru.netology.rest;public class MobileBankApiTestV4 {
+package ru.netology.rest;
+
+import io.restassured.module.jsv.JsonSchemaValidator;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
+
+class MobileBankApiTestV4 {
+
+    @Test
+    void shouldReturnDemoAccounts() {
+        // Given - When - Then
+        // Предусловия
+        given()
+                .baseUri("http://localhost:9999/api/v1")
+                // Выполняемые действия
+                .when()
+                .get("/demo/accounts")
+                // Проверки
+                .then()
+                .statusCode(200)
+                .body(matchesJsonSchemaInClasspath("accounts.schema.json"))
+                // static import для JsonSchemaValidator.matchesJsonSchemaInClasspath
+                .body(JsonSchemaValidator.matchesJsonSchemaInClasspath("accounts.schema.json"))
+        ;
+    }
 }
